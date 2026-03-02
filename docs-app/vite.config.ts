aa124932084d5130ed14f72b7acb2c3dc5162050
Jay Buensuceso <jbuens001@gmail.com>
2026-03-02 01:08:05 -0800

    added docs subproject

--- docs-app/vite.config.ts
+++ docs-app/vite.config.ts
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // Tells Vite where the index.html is for this project
+  root: "./", 
+  // Ensures assets load from the root of the subdomain
+  base: "/", 
+  build: {
+    // Output to a temporary folder for the Action to pick up
+    outDir: "dist", 
+    emptyOutDir: true,
+  },
 });
