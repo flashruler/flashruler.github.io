07341567d3e874cdbe76f2a2b683b4c749fa4fb9
Jay Buensuceso <jbuens001@gmail.com>
2026-03-02 01:45:46 -0800

    fixed project automatically directing to docs

--- docs-app/vite.config.ts
+++ docs-app/vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [react()],
   // Tells Vite where the index.html is for this project
   root: "./", 
-  // Ensures assets load from the root of the subdomain
-  base: "/", 
+  // Ensures assets load from the docs subpath
+  base: "/docs/", 
   build: {
     // Output to a temporary folder for the Action to pick up
     outDir: "dist", 
